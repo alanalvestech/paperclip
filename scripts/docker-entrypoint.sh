f0defd186c7b80f4480da83c699051cad31ddaae
Alan Alves <606570+alanalvestech@users.noreply.github.com>
2026-04-13 22:18:06 -0300

    fix: correct shebang and entrypoint encoding

--- scripts/docker-entrypoint.sh
+++ scripts/docker-entrypoint.sh
@@ -1,16 +1,14 @@
-#\!/bin/sh
+#!/bin/sh
 set -e
 
 PUID=${USER_UID:-1000}
 PGID=${USER_GID:-1000}
 
 if [ "$(id -u node)" -ne "$PUID" ]; then
-    echo "Updating node UID to $PUID"
     usermod -o -u "$PUID" node
 fi
 
 if [ "$(id -g node)" -ne "$PGID" ]; then
-    echo "Updating node GID to $PGID"
     groupmod -o -g "$PGID" node
     usermod -g "$PGID" node
 fi
